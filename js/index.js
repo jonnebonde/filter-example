15241e5214a4909b9a8803d855fe9bd1d9c4b3ce
cnnrbrn <connorbobrien@gmail.com>
2020-09-13 09:53:51 +0200

    Add first files

---
+++ js/index.js
@@ -0,0 +1,36 @@
+import teams from "./data/teams.js";
+
+const teamContainer = document.querySelector(".team-container");
+const search = document.querySelector(".search");
+
+let teamsToRender = teams;
+
+function renderTeams() {
+    teamContainer.innerHTML = "";
+
+    teamsToRender.forEach(function (team) {
+        teamContainer.innerHTML += `<div class="team">
+                                        <h4>${team.full_name}</h4>
+                                    </div>`;
+    });
+}
+
+renderTeams();
+
+search.onkeyup = function () {
+    // console.log(event);
+
+    const searchValue = event.target.value.trim().toLowerCase();
+
+    const filteredTeams = teams.filter(function (team) {
+        if (team.full_name.toLowerCase().startsWith(searchValue)) {
+            return true;
+        }
+    });
+
+    console.log(filteredTeams);
+
+    teamsToRender = filteredTeams;
+
+    renderTeams();
+};

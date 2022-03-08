9449a83a43beb28f21100a960b5e3be45c7eed59
jonnebonde <jonnebonde@gmail.com>
2022-03-08 14:16:16 +0100

    enter

--- js/index.js
+++ js/index.js
@@ -5,30 +5,39 @@ const search = document.querySelector(".search");
 
 let teamsToRender = teams;
 
+teamsToRender.forEach(function(team) {
+    teamContainer.innerHTML += `<div class="team">
+                                    <h4>${team.full_name}</h4>
+                                </div>`;
+});
+
+
 function renderTeams() {
+
     teamContainer.innerHTML = "";
 
-    teamsToRender.forEach(function (team) {
+    teamsToRender.forEach(function(team) {
         teamContainer.innerHTML += `<div class="team">
                                         <h4>${team.full_name}</h4>
                                     </div>`;
     });
+
 }
 
 renderTeams();
 
-search.onkeyup = function (event) {
-    // console.log(event);
+search.onkeyup = function() {
+    console.log(event)
 
     const searchValue = event.target.value.trim().toLowerCase();
 
-    const filteredTeams = teams.filter(function (team) {
-        if (team.full_name.toLowerCase().startsWith(searchValue)) {
+    const filteredTeams = teams.filter(function(team) {
+        if(team.full_name.toLocaleLowerCase().startsWith(searchValue)) {
             return true;
         }
-    });
+    })
 
-    console.log(filteredTeams);
+    console.table(filteredTeams)
 
     teamsToRender = filteredTeams;
 
